25fc4c094486056688a8c31d6b0b21a24d80585e
devinda.jay <devinda.jay.me@gmail.com>
2025-05-16 22:02:16 +0530

    create a method to retrive candidate by id

--- backend/src/models/candidateModel.js
+++ backend/src/models/candidateModel.js
@@ -16,7 +16,7 @@ const getAllCandidates = async () =>{
 
         sql += ' LIMIT ? OFFSET ?';
         params.push(parseInt(limit), parseInt(offset));
-        
+
         const validSortFields = ['id', 'name', 'applicationDate', 'overallScore'];
         const sortField = validSortFields.includes(sortBy) ? sortBy : 'id';
         sql += ` ORDER BY ${sortField} ASC`;
@@ -33,5 +33,39 @@ const getAllCandidates = async () =>{
     }catch (err){
         console.error('Error fetching candidates:', err.message);
         throw new Error('Failed to fetch candidates', err.message);
+    }
+}
+
+const getCandidateById = async (id) =>{
+    try{
+        let sql = 'SELECT * FROM candidates WHERE id = ?';
+        const params = [id];
+        const candidate = await new Promise ((resolve, reject) => {
+            db.get(sql, params , (err, row) =>{
+                if (err){
+                    return reject(new Error('Failed to fetch candidate by ID: ' + err.message));
+                }
+                resolve(row);
+            });
+        });
+
+        if (!candidate) {
+            throw new Error('Candidate not found with ID: ' + id);
+        }
+
+        return candidate;
+
+    }catch (err){
+        console.error('Error fetching candidate by ID:', err.message);
+        throw new Error('Failed to fetch candidate by ID', err.message);
+    }
+};
+
+const createCandidate = async (candidateData) =>{
+    try{
+        const { name}
+    } catch (err) {
+        console.error('Error creating candidate:', err.message);
+        throw new Error('Failed to create candidate', err.message);
     }
 }
