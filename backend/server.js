28ac3606d0343b13862e811da21bf619fbca0970
devinda.jay <devinda.jay.me@gmail.com>
2025-05-17 14:47:31 +0530

    finalize ther server

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./src/config/dbConfig');
+const db = require("./src/config/dbConfig");
+const candidateRoutes = require("./src/routes/candidateRoutes");
+const cors = require("cors");
 
-const dotenv= require('dotenv').config();
- 
+const dotenv = require("dotenv").config();
+
 const PORT = process.env.PORT || 5000;
 
-app.get('/api/test', (req,res) =>{
-    res.send('Test API route successfully');
-})
+app.use(cors());
+
+app.use(express.json());
+
+app.use('api/candidates', candidateRoutes);
 
 app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}`);
-}); 
+  console.log(`App is running on port ${PORT}`);
+});
